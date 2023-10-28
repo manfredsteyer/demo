677f03fc17310e71b74c8d0d27c82be797e159b1
Manfred Steyer <manfred.steyer@gmx.net>
2023-10-29 01:22:54 +0200

    feat(ui-common): add test component

---
+++ libs/ui-common/src/lib/Test/test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'release-demo-test',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css'],
+})
+export class TestComponent {}
